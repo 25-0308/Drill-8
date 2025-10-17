e7380ff90757e1a9e3063dac2a6bf30d776223ec
25-0308 <00rhkrtmdwns@hanmail.net>
2025-10-17 22:41:51 +0900

    경계 처리 추가

--- boy.py
+++ boy.py
@@ -52,6 +52,14 @@ class AutoRun:
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
         self.boy.x += self.boy.dir * 5
+        if self.boy.x < 0:
+            self.boy.x = 0
+            self.boy.dir = 2
+            self.boy.face_dir = 1
+        elif self.boy.x > 800:
+            self.boy.x = 800
+            self.boy.dir = -2
+            self.boy.face_dir = -1
 
     def draw(self):
         if self.boy.face_dir == 1: # right
